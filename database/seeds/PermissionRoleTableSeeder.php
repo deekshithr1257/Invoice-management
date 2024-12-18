a19d48d91b1196ed9778c1b16de34e81c9c710f3
deekshithr1257 <drdeeshu@gmail.com>
2024-12-18 18:20:02 +0530

    payment permissions added for admin

--- database/seeds/PermissionRoleTableSeeder.php
+++ database/seeds/PermissionRoleTableSeeder.php
@@ -11,7 +11,7 @@ class PermissionRoleTableSeeder extends Seeder
         $admin_permissions = Permission::all();
         Role::findOrFail(1)->permissions()->sync($admin_permissions->pluck('id'));
         $store_manager_permissions = $admin_permissions->filter(function ($permission) {
-            return substr($permission->title, 0, 5) != 'user_' && substr($permission->title, 0, 5) != 'role_' && substr($permission->title, 0, 11) != 'permission_'  && substr($permission->title, 0, 7) != 'payment_';
+            return substr($permission->title, 0, 5) != 'user_' && substr($permission->title, 0, 5) != 'role_' && substr($permission->title, 0, 11) != 'permission_';
         });
         Role::findOrFail(2)->permissions()->sync($store_manager_permissions);
     }

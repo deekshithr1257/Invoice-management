0285fb050447d0f52ab4fd7228fdfdea9bcaa3df
deekshithr1257 <drdeeshu@gmail.com>
2024-12-24 18:46:32 +0530

    payment type seeder added

---
+++ database/seeds/PaymentTypesTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\PaymentType;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class PaymentTypesTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $paymentTypes = [
+            [
+                'id'             => 1,
+                'name'           => 'Admin',
+                'created_by'     => 1,
+                'created_at'     => '2024-12-24 19:21:30',
+                'updated_at'     => '2024-12-24 19:21:30',
+            ],
+            [
+                'id'             => 2,
+                'name'           => 'Admin',
+                'created_by'     => 1,
+                'created_at'     => '2024-12-24 19:21:30',
+                'updated_at'     => '2024-12-24 19:21:30',
+            ],
+            [
+                'id'             => 3,
+                'name'           => 'Admin',
+                'created_by'     => 1,
+                'created_at'     => '2024-12-24 19:21:30',
+                'updated_at'     => '2024-12-24 19:21:30',
+            ],
+        ];
+
+        PaymentType::insert($paymentTypes);
+    }
+}

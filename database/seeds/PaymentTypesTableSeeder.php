5143fe5ec96c3804ea31e820074075e1f8bbeb12
deekshithr1257 <drdeeshu@gmail.com>
2024-12-24 18:48:40 +0530

    payment type seeder updated

--- database/seeds/PaymentTypesTableSeeder.php
+++ database/seeds/PaymentTypesTableSeeder.php
@@ -11,21 +11,21 @@ class PaymentTypesTableSeeder extends Seeder
         $paymentTypes = [
             [
                 'id'             => 1,
-                'name'           => 'Admin',
+                'name'           => 'Cash',
                 'created_by'     => 1,
                 'created_at'     => '2024-12-24 19:21:30',
                 'updated_at'     => '2024-12-24 19:21:30',
             ],
             [
                 'id'             => 2,
-                'name'           => 'Admin',
+                'name'           => 'UPI',
                 'created_by'     => 1,
                 'created_at'     => '2024-12-24 19:21:30',
                 'updated_at'     => '2024-12-24 19:21:30',
             ],
             [
                 'id'             => 3,
-                'name'           => 'Admin',
+                'name'           => 'Cheque',
                 'created_by'     => 1,
                 'created_at'     => '2024-12-24 19:21:30',
                 'updated_at'     => '2024-12-24 19:21:30',

f0b352c3ca71028ec2ba2891ba7e50dfddc0547c
deekshithr1257 <drdeeshu@gmail.com>
2025-01-27 20:40:24 +0530

    hide registration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,7 +47,7 @@
                                     </div>
                                 </div>
                             </form>
-                            <p class="mt-5 login-form__footer">Dont have account? <a href="{{ route('register') }}" class="text-primary">Sign Up</a> now</p>
+                            <!-- <p class="mt-5 login-form__footer">Dont have account? <a href="{{ route('register') }}" class="text-primary">Sign Up</a> now</p> -->
                         </div>
                     </div>
                 </div>
